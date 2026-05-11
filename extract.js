02383b836894c9ca7065f2c72f5d6362223406f9
Eramis <enesramisg@gmail.com>
2026-05-12 00:43:05 +0300

    chore: remove all unnecessary files, keep only server.js and package.json
    Removed debug scripts, test files, log files, Word documents, images,
    PWA assets, and private IDE/Netlify config files that are not needed
    for the WebSocket relay server to function.

--- extract.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('c:/Users/Enes/Desktop/Game Jam/index.html', 'utf8');
-const start = html.indexOf('<script>') + '<script>'.length;
-const end = html.indexOf('</script>');
-const js = html.substring(start, end);
-fs.writeFileSync('c:/Users/Enes/Desktop/Game Jam/test_check.js', js);
--- test_script.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('c:/Users/Enes/Desktop/Game Jam/index.html', 'utf8');
-const scriptPart = html.split('<script>')[1].split('</script>')[0];
-fs.writeFileSync('c:/Users/Enes/Desktop/Game Jam/test_check.js', scriptPart);
-try {
-    require('child_process').execSync('node "c:/Users/Enes/Desktop/Game Jam/test_check.js"', {stdio: 'inherit'});
-} catch(e) {
-    console.error("Syntax execution error.");
-}
